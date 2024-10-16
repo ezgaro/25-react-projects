b10615ac8c9296a4d4976a4f0af66f58f3bbad30
ezgaro <ezgaro2005@gmail.com>
2024-10-16 23:33:26 +0300

    Accordion

---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id:'1',
+    question: 'What are accordeon components ?',
+    answer: 'Accordeon components are user interface elements used for organizing data.'
+  },
+  {
+    id:'2',
+    question: 'What are accordeon components ?',
+    answer: 'Accordeon components are user interface elements used for organizing data.'
+  },
+  {
+    id:'3',
+    question: 'What are accordeon components ?',
+    answer: 'Accordeon components are user interface elements used for organizing data.'
+  },
+  {
+    id:'4',
+    question: 'What are accordeon components ?',
+    answer: 'Accordeon components are user interface elements used for organizing data.'
+  },
+]
+
+export default data;
