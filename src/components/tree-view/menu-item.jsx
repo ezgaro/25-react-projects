e34562e8b3752e59d0953266fe8188ca4cd7eaac
ezgaro <ezgaro2005@gmail.com>
2024-10-21 10:42:47 +0300

    adding some styles to the tree-view

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -15,7 +15,7 @@ export default function MenuItem({ item }) {
   console.log(displayCurrentChildren);
   return (
     <li>
-      <div style={{ display: "flex", gap: "20px" }}>
+      <div className="menu-item">
         <p>{item.label}</p>
         {item && item.children && item.children.length > 0 ? ( //if we have children put plus icon
           <span onClick={() => handleToggleChildren(item.label)}>
